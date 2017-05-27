dc5412cb1cc2f0a7851659a51c8b5789a0a24ff9
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2017-05-27 21:05:09 +0200

    Hash a string

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,16 +3,14 @@ defmodule Identicon do
   Documentation for Identicon.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
   end
+
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+  end
+
 end
